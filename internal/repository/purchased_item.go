4ca8144c1883686c8d93e08c431b4149755efd9e
nesangcode <k124k3n@gmail.com>
2025-01-26 15:11:49 +0700

    Revert "feat: add put producut"
    This reverts commit 5c89e789432af427805b1059925d032b27ca8afb.

--- internal/repository/purchased_item.go
+++ internal/repository/purchased_item.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"errors"
 
 	"github.com/bayuuat/tutuplapak/domain"
 	"github.com/bayuuat/tutuplapak/dto"
@@ -29,21 +28,21 @@ func NewPurchasedItem(db *sql.DB) PurchasedItemRepository {
 }
 
 func (d purchasedItemRepository) Save(ctx context.Context, purchasedItem *domain.PurchasedItem) (*domain.PurchasedItem, error) {
-	return &domain.PurchasedItem{}, errors.New("not implemented")
+	return nil, nil
 }
 
 func (d purchasedItemRepository) Update(ctx context.Context, userId string, purchasedItem goqu.Record) error {
-	return errors.New("not implemented")
+	return nil
 }
 
 func (d purchasedItemRepository) FindById(ctx context.Context, userId, id string) (purchasedItem domain.PurchasedItem, err error) {
-	return domain.PurchasedItem{}, errors.New("not implemented")
+	return nil, nil
 }
 
 func (d purchasedItemRepository) Delete(ctx context.Context, userId, id string) error {
-	return errors.New("not implemented")
+	return nil
 }
 
 func (d purchasedItemRepository) FindAllWithFilter(ctx context.Context, filter *dto.PurchasedItemFilter, userId string) ([]domain.PurchasedItem, error) {
-	return []domain.PurchasedItem{}, errors.New("not implemented")
+	return nil, nil
 }
--- internal/service/product.go
+++ internal/service/product.go
@@ -2,95 +2,45 @@ package service
 
 import (
 	"context"
-	"errors"
-	"github.com/bayuuat/tutuplapak/domain"
+
 	"github.com/bayuuat/tutuplapak/dto"
 	"github.com/bayuuat/tutuplapak/internal/config"
 	"github.com/bayuuat/tutuplapak/internal/repository"
-	"log/slog"
-	"net/http"
-	"strconv"
-	"time"
 )
 
-type ProductService struct {
+type productService struct {
 	cnf               *config.Config
 	productRepository repository.ProductRepository
-	fileService       FileService
 }
 
-type ProductServicer interface {
+type ProductService interface {
 	GetProductsWithFilter(ctx context.Context, filter dto.ProductFilter, userId string) ([]dto.ProductData, int, error)
 	CreateProduct(ctx context.Context, req dto.ProductReq, userId string) (dto.ProductData, int, error)
-	PutProduct(ctx context.Context, req dto.ProductReq, id string) (dto.ProductData, int, error)
-	DeleteProduct(ctx context.Context, userId, id string) (dto.ProductData, int, error)
+	PatchProduct(ctx context.Context, req dto.UpdateProductReq, userId, id string) (dto.ProductData, int, error)
+	DeleteProduct(ctx context.Context, user_id, id string) (dto.ProductData, int, error)
 }
 
-func NewProductServicer(cnf *config.Config,
+func NewProduct(cnf *config.Config,
 	productRepository repository.ProductRepository,
-	fileService FileService) ProductServicer {
-	return ProductService{
+	productTypesRepository repository.ProductTypesRepository) ProductService {
+	return &productService{
 		cnf:               cnf,
 		productRepository: productRepository,
-		fileService:       fileService,
 	}
 }
 
-func (ds ProductService) GetProductsWithFilter(ctx context.Context, filter dto.ProductFilter, userId string) ([]dto.ProductData, int, error) {
-	return []dto.ProductData{}, 0, nil
+func (ds productService) GetProductsWithFilter(ctx context.Context, filter dto.ProductFilter, userId string) ([]dto.ProductData, int, error) {
+	return nil, nil, nil
 }
 
-func (ds ProductService) CreateProduct(ctx context.Context, req dto.ProductReq, userId string) (dto.ProductData, int, error) {
-	return dto.ProductData{}, 0, nil
+func (ds *productService) CreateProduct(ctx context.Context, req dto.ProductReq, userId string) (dto.ProductData, int, error) {
+	return nil, nil, nil
 }
 
-func (ds ProductService) PutProduct(ctx context.Context, req dto.ProductReq, productId string) (dto.ProductData, int, error) {
-	file, product, errorCode, err := ds.validateProduct(ctx, req, productId)
-	if err != nil {
-		return dto.ProductData{}, errorCode, err
-	}
-
-	err = ds.productRepository.Put(ctx, dto.Product(req))
-	if err != nil {
-		slog.ErrorContext(ctx, err.Error())
-		return dto.ProductData{}, http.StatusInternalServerError, err
-	}
-
-	return dto.ProductData{
-		ProductId:        strconv.Itoa(product.ProductID),
-		Name:             req.Name,
-		Category:         req.Category,
-		Qty:              req.Qty,
-		Price:            req.Price,
-		Sku:              req.Sku,
-		FileId:           strconv.Itoa(file.FileID),
-		FileUri:          file.FileURI,
-		FileThumbnailUri: file.FileThumbnailURI,
-		CreatedAt:        time.Now().Format(time.RFC3339Nano),
-		UpdatedAt:        time.Now().Format(time.RFC3339Nano),
-	}, 200, nil
+func (ds productService) PatchProduct(ctx context.Context, req dto.UpdateProductReq, userId, id string) (dto.ProductData, int, error) {
+	return nil, nil, nil
 }
 
-func (ds ProductService) validateProduct(ctx context.Context, req dto.ProductReq, productId string) (domain.File, domain.Product, int, error) {
-	file, err := ds.fileService.GetFileId(ctx, req.FileId)
-	if err != nil {
-		return domain.File{}, domain.Product{}, http.StatusInternalServerError, err
-	}
-	if file.FileID == 0 {
-		return domain.File{}, domain.Product{}, http.StatusNotFound, errors.New("not found")
-	}
-
-	product, err := ds.productRepository.FindById(ctx, productId)
-	if err != nil {
-		return domain.File{}, domain.Product{}, http.StatusInternalServerError, err
-	}
-	if product.ProductID == 0 {
-		return domain.File{}, domain.Product{}, http.StatusNotFound, errors.New("not found")
-	}
-
-	return file, product, 200, nil
-}
-
-func (ds ProductService) DeleteProduct(ctx context.Context, userId, id string) (dto.ProductData, int, error) {
-	return dto.ProductData{}, 0, nil
+func (ds productService) DeleteProduct(ctx context.Context, userId, id string) (dto.ProductData, int, error) {
+	return nil, nil, nil
 }
